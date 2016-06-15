db1f9ed7570796e52a6a318b2d67753544fd62b5
Maciek Stasieluk <macrusher@gmail.com>
2016-06-16 01:58:23 +0200

    Improve error handling

--- validated-publication.js
+++ validated-publication.js
@@ -1,28 +1,35 @@
 import {Meteor} from 'meteor/meteor';
 import {Match, check} from 'meteor/check';
 
+/**
+ * Create new publication
+ * @param {{}} options - publication configuration
+ * @param {String} options.name - name of the publication
+ * @param {Function[]} [options.mixins=[]] - mixins to enhance the publication
+ * @param {Function} options.validate - function to validate arguments
+ * @param {Function} options.run - publication body
+ * @param {Object} [options.connection=Meteor] - Connection object
+ */
 export class ValidatedPublication {
 
-    /**
-     * Create new publication
-     * @param {String} options.name - name of the publication
-     * @param {Function[]} [options.mixins=[]] - mixins to enhance the publication
-     * @param {Function} options.validate - function to validate arguments
-     * @param {Function} options.run - publication body
-     * @param {Object} [options.connection=Meteor]
-     */
-    constructor(options) {
+    constructor (options = {}) {
 
-        // Default values
+        // Apply default values
         _.defaults(options, {
             mixins: [],
             connection: Meteor
         });
 
-        // Verify initial options (one at a time for easier debugging)
-        check(options.name, String);
-        check(options.mixins, [Function]);
-        check(options.connection, Object);
+        // Verify initial options
+        if (!Match.test(options.name, String)) {
+            throw new Error('ValidatedPublication require a name');
+        }
+        if (!Match.test(options.mixins, [Function])) {
+            throw new Error(`Mixins must be an array of functions for ValidatedPublication named ${options.name}`);
+        }
+        if (!Match.test(options.connection, Object)) {
+            throw new Error(`You need to specify a connection object for ValidatedPublication named ${options.name}`);
+        }
 
         // Let the mixins do the magic
         options = applyMixins(options, options.mixins);
@@ -32,12 +39,22 @@ export class ValidatedPublication {
             options.validate = () => {};
         }
 
-        // Final verification (one at a time for easier debugging)
-        check(options.name, String);
-        check(options.mixins, [Function]);
-        check(options.validate, Function);
-        check(options.run, Function);
-        check(options.connection, Object);
+        // Final verification, mixins may broke something
+        if (!Match.test(options.name, String)) {
+            throw new Error('ValidatedPublication require a name');
+        }
+        if (!Match.test(options.mixins, [Function])) {
+            throw new Error(`Mixins must be an array of functions for ValidatedPublication named ${options.name}`);
+        }
+        if (!Match.test(options.validate, Function)) {
+            throw new Error(`You need to provide a validate function for ValidatedPublication named ${options.name}`);
+        }
+        if (!Match.test(options.run, Function)) {
+            throw new Error(`You need to provide a run function for ValidatedPublication named ${options.name}`);
+        }
+        if (!Match.test(options.connection, Object)) {
+            throw new Error(`You need to specify a connection object for ValidatedPublication named ${options.name}`);
+        }
 
         // Attach all options to the instance
         Object.assign(this, options);
@@ -45,28 +62,28 @@ export class ValidatedPublication {
         const publication = this;
 
         // Create the real publication
-        this.connection.publish(this.name, function (args) {
+        this.connection.publish(this.name, function validatedPublication (args) {
             // Silence audit-argument-checks since arguments are always checked when using this package
             check(args, Match.Any);
             return publication._execute(this, args);
         });
     }
 
-    _execute(invocation = {}, args = {}) {
+    _execute (invocation = {}, args = {}) {
         // Add `this.name` to reference the publication name
         invocation.name = this.name;
 
         const validateResult = this.validate.bind(invocation)(args);
 
         if (typeof validateResult !== 'undefined') {
-            throw new Error(`Returning from validate doesn't do anything; perhaps you meant to throw an error?`);
+            throw new Error(`Returning from validate doesn't do anything; perhaps you meant to throw an error? Check ValidatedMethod named ${this.name}`);
         }
 
         return this.run.bind(invocation)(args);
     }
 }
 
-function applyMixins(options, mixins) {
+function applyMixins (options, mixins) {
     // Save name of the publication here, so we can attach it to potential error messages
     const {name} = options;
 
